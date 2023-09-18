792c10653e52610439a41ec65d932e5a6d457b9b
idoh-spherex <ido.h@sphere-x.xyz>
2023-09-18 18:20:15 +0300

    Added sub upgrade function.

--- src/ProtectedProxies/ProtectedTransparentUpgradeableSubProxy.sol
+++ src/ProtectedProxies/ProtectedTransparentUpgradeableSubProxy.sol
@@ -8,15 +8,26 @@ import "openzeppelin/Proxy/transparent/TransparentUpgradeableProxy.sol";
 import {SphereXProtectedSubProxy} from "../SphereXProtectedSubProxy.sol";
 import {SphereXProtectedProxy} from "../SphereXProtectedProxy.sol";
 
+interface ISphereXProtectedSubProxy {
+    function subUpgradeTo(address newImplementation) external;
+}
 
+
+// TODO - we should remember that it is not really transparent because SphereXProtectedProxy brings public functions.
 contract ProtectedTransparentUpgradeableSubProxy is SphereXProtectedSubProxy, TransparentUpgradeableProxy {
     constructor(address _logic, address admin_, bytes memory _data)
-        SphereXProtectedSubProxy(msg.sender, address(0), address(0), _logic, admin_)
+        SphereXProtectedSubProxy(msg.sender, address(0), address(0), _logic)
         TransparentUpgradeableProxy(_logic, admin_, _data)
     {}
 
     function _fallback() internal virtual override(Proxy, TransparentUpgradeableProxy) {
-        TransparentUpgradeableProxy._fallback();
+        if (msg.sender == _getAdmin() && msg.sig == ISphereXProtectedSubProxy.subUpgradeTo.selector) {
+            address newImplementation = abi.decode(msg.data[4:], (address));
+            subUpgradeTo(newImplementation);
+        }
+        else {
+            TransparentUpgradeableProxy._fallback();
+        }
     }
 
     function _delegate(address implementation) internal virtual override(Proxy, SphereXProtectedProxy) {
--- src/SphereXProtectedSubProxy.sol
+++ src/SphereXProtectedSubProxy.sol
@@ -8,14 +8,16 @@ import {SphereXProtectedProxy} from "./SphereXProtectedProxy.sol";
 abstract contract SphereXProtectedSubProxy is SphereXProtectedProxy {
 
     bytes32 private constant _SPHEREX_IMPLEMENTATION_SLOT = bytes32(uint256(keccak256("eip1967.spherex.implementation_slot")) - 1);
-    bytes32 private constant _SPHEREX_PROXT_ADMIN_SLOT = bytes32(uint256(keccak256("eip1967.spherex.proxy_admin_slot")) - 1);
 
-    constructor(address admin, address operator, address engine, address _logic, address _proxy_admin) SphereXProtectedProxy(admin, operator, engine) {
+    constructor(address admin, address operator, address engine, address _logic) SphereXProtectedProxy(admin, operator, engine) {
         _setAddress(_SPHEREX_IMPLEMENTATION_SLOT, _logic);
-        _setAddress(_SPHEREX_PROXT_ADMIN_SLOT, _proxy_admin);
     }
 
     function _implementation() internal view virtual override returns (address impl) {
         return _getAddress(_SPHEREX_IMPLEMENTATION_SLOT);
+    }
+
+    function subUpgradeTo(address newImplementation) internal {
+        _setAddress(_SPHEREX_IMPLEMENTATION_SLOT, newImplementation);
     }
 }
