c5eadf3a04b710d620384aa958ce564867ebf4dd
idoh-spherex <ido.h@sphere-x.xyz>
2023-10-30 13:42:31 +0200

    Added changes for npm publish.

--- src/SphereXEngine.sol
+++ src/SphereXEngine.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.17;
 
-import {AccessControlDefaultAdminRules} from "openzeppelin-contracts/access/AccessControlDefaultAdminRules.sol";
-import {ISphereXEngine} from "spherex-protect-contracts/ISphereXEngine.sol";
+import {AccessControlDefaultAdminRules} from "@openzeppelin/contracts/access/AccessControlDefaultAdminRules.sol";
+import {ISphereXEngine} from "@spherex-xyz/contracts/src/ISphereXEngine.sol";
 
 /**
  * @title SphereX Engine
