c95af7b30ce7af381210cc0ff65d4cc02783e4a4
Michael R <michael.r@spherex.xyz>
2024-08-05 11:47:28 +0300

    updated factory

--- src/ProtectedProxies/MinimalProtectedProxyFactory.sol
+++ src/ProtectedProxies/MinimalProtectedProxyFactory.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import {SphereXProtected} from "../SphereXProtected.sol";
+import {SphereXConfiguration} from "../SphereXConfiguration.sol";
 import {SpherexProtetedMinimalProxy} from "./ProtectedMinimalProxy.sol";
 
 // TODO: add event for deploying
@@ -11,28 +11,48 @@ import {SpherexProtetedMinimalProxy} from "./ProtectedMinimalProxy.sol";
 /**
  * @title A factory contract that deploys the SpherexProtetedMinimalProxy contracts.
  */
-contract MinimalProtectedProxyFactory is SphereXProtected {
+contract MinimalProtectedProxyFactory is SphereXConfiguration {
+
+    address private immutable IMPLEMENTATION;
+    address private immutable ALLOWED_DEPLOYER;
+    bytes4[] private /*immutable*/ PROTECTED_SIGS; // this is in effect immutable.
+
+    constructor(address allowedDeployer, address implementation, bytes4[] memory protectedSigs ) SphereXConfiguration(msg.sender, address(0), address(0)) {
+        ALLOWED_DEPLOYER = allowedDeployer;
+        IMPLEMENTATION = implementation;
+        PROTECTED_SIGS = protectedSigs;
+    }
+
+    event DeployedMinimalPRotectedProxy(address proxy_address, address implementation);
+
+
+    modifier onlyAllowedDeployer() {
+        require(msg.sender == ALLOWED_DEPLOYER, "Only Allowed Deployer");
+        _;
+    }
+
+    
 
     /**
      * Deploys a new protected minimal proxy. Sets the inital protected values (admin, operator and engie) to equal those of the factory.
      *  If the current engine is not the null address, also updates the engine about a valid new sender
      * @dev notice the deploy function itself is NOT PROTECTED
      */
-    function deploy(address implementation) virtual public returns (address) {
+    function deploy() virtual public onlyAllowedDeployer returnsIfNotActivated returns (address proxyAddress) {
         address engineAddress = sphereXEngine();
         SpherexProtetedMinimalProxy minimalProxy = new SpherexProtetedMinimalProxy(
             sphereXAdmin(), 
             sphereXOperator(), 
             engineAddress, 
-            implementation
+            IMPLEMENTATION
         );
-        address proxyAddress = address(minimalProxy);
+        proxyAddress = address(minimalProxy);
 
-        if (engineAddress != address(0)) {
-            _addAllowedSenderOnChain(address(proxyAddress));
-        }
-        // TODO: log event
-        return proxyAddress;
+        _addAllowedSenderOnChain(address(proxyAddress));
+        minimalProxy.addProtectedFuncSigs(PROTECTED_SIGS);
+
+        emit DeployedMinimalPRotectedProxy(proxyAddress, IMPLEMENTATION);
+        
     }
 
 }
