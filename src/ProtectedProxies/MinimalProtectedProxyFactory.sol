408e30d77afb42eabecccf144105e5989a1d2ef4
Michael R <michael.r@spherex.xyz>
2024-08-08 13:06:56 +0300

    fixed CR

--- src/ProtectedProxies/MinimalProtectedProxyFactory.sol
+++ src/ProtectedProxies/MinimalProtectedProxyFactory.sol
@@ -14,24 +14,30 @@ import {SpherexProtetedMinimalProxy} from "./ProtectedMinimalProxy.sol";
 contract MinimalProtectedProxyFactory is SphereXConfiguration {
 
     address private immutable IMPLEMENTATION;
-    address private immutable ALLOWED_DEPLOYER;
     bytes4[] private /*immutable*/ PROTECTED_SIGS; // this is in effect immutable.
+    
+    address private  _allowedDeployer;
 
-    constructor(address allowedDeployer, address implementation, bytes4[] memory protectedSigs ) SphereXConfiguration(msg.sender, address(0), address(0)) {
-        ALLOWED_DEPLOYER = allowedDeployer;
+    constructor(address implementation, bytes4[] memory protectedSigs ) SphereXConfiguration(msg.sender, address(0), address(0)) {
         IMPLEMENTATION = implementation;
         PROTECTED_SIGS = protectedSigs;
+
+        _allowedDeployer = address(0);
     }
 
     event DeployedMinimalProtectedProxy(address proxy_address, address implementation);
 
 
     modifier onlyAllowedDeployer() {
-        require(msg.sender == ALLOWED_DEPLOYER, "Only Allowed Deployer");
+        require(_allowedDeployer != address(0), "Must define Allowed deployer");
+        require(msg.sender == _allowedDeployer, "Only Allowed Deployer");
         _;
     }
 
-    
+    function initializeAllowedDeployer(address allowedDeployer) external {
+        require(_allowedDeployer == address(0), "Allowed deployer already exists");
+        _allowedDeployer = allowedDeployer;
+    }
 
     /**
      * Deploys a new protected minimal proxy. Sets the inital protected values (admin, operator and engie) to equal those of the factory.
